b8f67eb6682755d988c7caed752d310d2c4031d4
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-07-26 15:56:59 +0300

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,114 @@
-import fs from "fs";
 import axios from "axios";
-import Excel, {Workbook, Worksheet} from "exceljs";
+import * as dotenv from 'dotenv';
+import Excel, {Worksheet} from "exceljs";
+import {GoogleSpreadsheet} from 'google-spreadsheet';
+//import fs from "fs";
+// const sheets = google.sheets('v4');
 // const { google } = require('googleapis');
 // const {authenticate} = require('@google-cloud/local-auth');
-// const sheets = google.sheets('v4');
-import {GoogleSpreadsheet} from 'google-spreadsheet';
-import * as dotenv from 'dotenv';
+dotenv.config();
+
 interface IEntry {
-    API: string,
-    Description: string,
-    Auth: string,
-    HTTPS: boolean,
-    Cors: string,
-    Link: string,
-    Category: string
+    API: string;
+    Description: string;
+    Auth: string;
+    HTTPS: boolean;
+    Cors: string;
+    Link: string;
+    Category: string;
 }
 
-dotenv.config()
-
 class CreateReport {
-    private workbook: Workbook;
-    constructor() {
+    private workbook: Excel.Workbook;
+    private doc: GoogleSpreadsheet;
+    constructor(sheetId: string) {
         this.workbook = new Excel.Workbook();
+        this.doc = new GoogleSpreadsheet(sheetId);
     }
-    static async getDataFromApi(url: string) {
+
+    public async getDataFromApi(url: string) {
         const response = await axios.get(url);
         return response.data.entries
-            .filter((entry: IEntry) => entry.HTTPS === true)
-            .sort((a: IEntry, b: IEntry) => a.API.localeCompare(b.API));
-    }
+            .filter((entry: IEntry) => entry.HTTPS) // if entry.HTTPS === true
+            .sort((entryA: IEntry, entryB: IEntry) => entryA.API.localeCompare(entryB.API));
+    };
 
-    static async getArrayOfHeaders(data: IEntry[]) {
+    public async getArrayOfHeaders(data: IEntry[]): Promise<string[]> {
         return [...new Set(data.flatMap((item: IEntry) => Object.keys(item)))]
-    }
+    };
 
-    static async createReportByExcel(url: string, fgColor: string) {
-        const workbook = new Excel.Workbook();
-        const worksheet: Worksheet = workbook.addWorksheet("Metacommerce");
-        const data = await this.getDataFromApi(url)
+    public async createReportByExcel(url: string, title: string, fgColor: string): Promise<void> {
+        const worksheet: Worksheet = this.workbook.addWorksheet(title);
+        const data = await this.getDataFromApi(url);
         const arrayHeaders = await this.getArrayOfHeaders(data);
-        worksheet.columns = arrayHeaders.map((header) => {
-            return {header: `${header}`, key: `${header}`, width: 20}
+        worksheet.columns = arrayHeaders.map((header: string) => {
+            return {header: `${header}`, key: `${header}`, width: 20};
         });
         arrayHeaders.map((header: string, index: number) => {
-            worksheet.getCell(1,index + 1).fill = {
+            worksheet.getCell(1, index + 1).fill = {
                 type: 'pattern',
                 pattern:'solid',
                 fgColor:{ argb: fgColor }
             };
-            worksheet.getCell(1,index + 1).border = {
+            worksheet.getCell(1, index + 1).border = {
                 top: {style: 'medium', color: {argb: "000000"}},
                 left: {style: 'medium', color: {argb: "000000"}},
                 bottom: {style: 'medium', color: {argb: "000000"}},
                 right: {style: 'medium', color: {argb: "000000"}},
-            }
+            };
         });
         data.forEach((entry: IEntry) => {
             worksheet.addRow([entry.API, entry.Description, entry.Auth, entry.HTTPS, entry.Cors, { text: entry.Link, hyperlink: entry.Link }, entry.Category]);
         });
 
-        await workbook.xlsx.writeFile('export.xlsx');
+        await this.workbook.xlsx.writeFile('report.xlsx');
     }
 
-    static async createSheet(url: string) {
+    public async createSheet(url: string, title: string, GOOGLE_SERVICE_ACCOUNT_EMAIL: string, GOOGLE_PRIVATE_KEY: string): Promise<void> {
         const data = await this.getDataFromApi(url);
         const arrayHeaders = await this.getArrayOfHeaders(data);
-        const doc = new GoogleSpreadsheet('1pUVZF8COsRBrzhfK2IIX6oSnQxyurD8BWhQfTFzX5Ow');
-        const { GOOGLE_PRIVATE_KEY, GOOGLE_SERVICE_ACCOUNT_EMAIL } = process.env;
-        await doc.useServiceAccountAuth({
-            client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL as string,
-            private_key: GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        await this.doc.useServiceAccountAuth({
+            client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL.toString(),
+            private_key: GOOGLE_PRIVATE_KEY.toString()!.replace(/\\n/g, '\n'),
         });
-        await doc.loadInfo()
+        await this.doc.loadInfo();
 
-        const sheet = await doc.addSheet({ // Создаем новый лист
-            title: 'Metacommerce',
+        const sheet = await this.doc.addSheet({ // Create new sheet
+            title: title,
             headerValues: arrayHeaders,
+            tabColor: {
+                red: 1.0,
+                green: 0.3,
+                blue: 0.4,
+                alpha: 1.0
+            }
         });
-        const rows = data.reduce(async (acc: IEntry[], entry: IEntry) => {
-            let item: IEntry = { API: entry.API, Description: entry.Description, Auth: entry.Auth, HTTPS: entry.HTTPS, Cors: entry.Cors, Link: entry.Link, Category: entry.Category }
-            acc.push(item)
-            return acc
-        }, [])
-        await sheet.addRows(rows)
+        const rows = data.reduce((acc: IEntry[], entry: IEntry) => {
+            let item: IEntry = {
+                API: entry.API,
+                Description: entry.Description,
+                Auth: entry.Auth,
+                HTTPS: entry.HTTPS,
+                Cors: entry.Cors,
+                Link: entry.Link,
+                Category: entry.Category
+            };
+            acc.push(item);
 
+            return acc;
+        }, []);
+
+        await sheet.addRows(rows);
     }
 }
- // CreateReport.createReportByExcel("https://api.publicapis.org/entries", "cccccc").then(() => {
- //     console.log("Successful")
- // })
-
-CreateReport.createSheet("https://api.publicapis.org/entries").then(() => {
+const { GOOGLE_SERVICE_ACCOUNT_EMAIL, GOOGLE_PRIVATE_KEY, SHEET_ID } = process.env;
+const createReport = new CreateReport(SHEET_ID as string);
+createReport.createSheet("https://api.publicapis.org/entries", "Metacommerce", GOOGLE_SERVICE_ACCOUNT_EMAIL as string, GOOGLE_PRIVATE_KEY as string).then(() => {
     console.log("Successful")
 });
+// createReport.createReportByExcel("https://api.publicapis.org/entries", "Metacommerce", "ccccc").then(() => {
+//     console.log("Successful")
+// });
 // const doc = new GoogleSpreadsheet();
 // const { GOOGLE_PRIVATE_KEY, GOOGLE_SERVICE_ACCOUNT_EMAIL } = process.env;
 //
