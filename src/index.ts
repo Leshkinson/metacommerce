6f967f0a7346f87eb282c8f77cee00cb5373e9c8
Leshkinson <lopatkool93@gmail.com>
2023-07-30 22:51:08 +0300

    adding DataHandler class

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,15 @@ interface IEntry {
     Category: string;
 }
 
-class CreateReport {
-    private workbook: Excel.Workbook;
-    private doc: GoogleSpreadsheet;
-    constructor(sheetId: string) {
-        this.workbook = new Excel.Workbook();
-        this.doc = new GoogleSpreadsheet(sheetId);
-    }
+interface SpreadsheetRow {
+    [header: string]: string | number | boolean;
+}
 
-    public async getDataFromApi(url: string) {
+interface DynamicEntity extends IEntry, SpreadsheetRow {
+}
+
+class DataHandler {
+    public async getDataFromApi(url: string): Promise<IEntry[]> {
         const response = await axios.get(url);
         return response.data.entries
             .filter((entry: IEntry) => entry.HTTPS) // if entry.HTTPS === true
@@ -33,6 +33,15 @@ class CreateReport {
     public async getArrayOfHeaders(data: IEntry[]): Promise<string[]> {
         return [...new Set(data.flatMap((item: IEntry) => Object.keys(item)))]
     };
+}
+
+class ReportByExcel extends DataHandler {
+    private workbook: Excel.Workbook;
+    constructor() {
+        super();
+        this.workbook = new Excel.Workbook();
+
+    }
 //1. Создать с помощью nodeJS отчет в Excel
     public async createReportByExcel(url: string, title: string, fgColor: string): Promise<void> {
         const worksheet: Worksheet = this.workbook.addWorksheet(title);
@@ -59,6 +68,14 @@ class CreateReport {
         });
 
         await this.workbook.xlsx.writeFile('report.xlsx');
+    }
+}
+
+class ReportByGoogleSheet extends DataHandler {
+    private doc: GoogleSpreadsheet;
+    constructor(sheetId: string) {
+        super();
+        this.doc = new GoogleSpreadsheet(sheetId);
     }
 //2. Создать с помощью nodeJS отчет в Google Sheets
     public async createSheet(url: string, title: string, GOOGLE_SERVICE_ACCOUNT_EMAIL: string, GOOGLE_PRIVATE_KEY: string): Promise<void> {
@@ -93,46 +110,49 @@ class CreateReport {
             acc.push(item);
 
             return acc;
-        }, []);
+        }, []) as DynamicEntity[];
 
         await sheet.addRows(rows);
     }
 }
 //3. Создать с помощью nodeJS отчет в Google Sheets с помощью Google Apps Script
 //Скрипт
-function createReportDataFromAPI() {
-    // Забираем данные из API
-    const response = UrlFetchApp.fetch("https://api.publicapis.org/entries");
-    const data = JSON.parse(response.getContentText());
-
-    // Исключаем объекты с HTTPS: false
-    const filteredDataFromApi = data.entries
-        .filter(entry => entry.HTTPS) // if entry.HTTPS === true
-        .sort((entryA, entryB) => entryA.API.localeCompare(entryB.API));
-
-    // Получаем заголовки из API
-    //const arrayHeaders = [...new Set(data.map((item: IEntry) => Object.keys(item)).flat())];
-    const arrayHeaders = ["API", "Description", "Auth", "HTTPS", "Cors", "Link", "Category"];
-    // Получаем активный лист в Google Sheets
-    const sheet = SpreadsheetApp.getActiveSheet();
-
-    // Записываем заголовки
-    sheet.appendRow(arrayHeaders);
-
-    // Записываем данные из API
-    filteredDataFromApi.forEach((entry) => {
-        sheet.appendRow([entry.API, entry.Description, entry.Auth, entry.HTTPS, entry.Cors, entry.Link, entry.Category]);
-    });
-}
+// function createReportDataFromAPI() {
+//     // Забираем данные из API
+//     const response = UrlFetchApp.fetch("https://api.publicapis.org/entries");
+//     const data = JSON.parse(response.getContentText());
+//
+//     // Исключаем объекты с HTTPS: false
+//     const filteredDataFromApi = data.entries
+//         .filter(entry => entry.HTTPS) // if entry.HTTPS === true
+//         .sort((entryA, entryB) => entryA.API.localeCompare(entryB.API));
+//
+//     // Получаем заголовки из API
+//     //const arrayHeaders = [...new Set(data.map((item: IEntry) => Object.keys(item)).flat())];
+//     const arrayHeaders = ["API", "Description", "Auth", "HTTPS", "Cors", "Link", "Category"];
+//     // Получаем активный лист в Google Sheets
+//     const sheet = SpreadsheetApp.getActiveSheet();
+//
+//     // Записываем заголовки
+//     sheet.appendRow(arrayHeaders);
+//
+//     // Записываем данные из API
+//     filteredDataFromApi.forEach((entry) => {
+//         sheet.appendRow([entry.API, entry.Description, entry.Auth, entry.HTTPS, entry.Cors, entry.Link, entry.Category]);
+//     });
+// }
 
 const { GOOGLE_SERVICE_ACCOUNT_EMAIL, GOOGLE_PRIVATE_KEY, SHEET_ID } = process.env;
-const createReport = new CreateReport(SHEET_ID as string);
+const CreateReportByExcel = new ReportByExcel();
+const createReportByGoogleSheet = new ReportByGoogleSheet(SHEET_ID as string);
 
-createReport.createSheet("https://api.publicapis.org/entries", "Metacommerce", GOOGLE_SERVICE_ACCOUNT_EMAIL as string, GOOGLE_PRIVATE_KEY as string).then(() => {
+CreateReportByExcel.createReportByExcel("https://api.publicapis.org/entries", "Metacommerce", "ccccc").then(() => {
     console.log("Successful")
 });
-createReport.createReportByExcel("https://api.publicapis.org/entries", "Metacommerce", "ccccc").then(() => {
+
+createReportByGoogleSheet.createSheet("https://api.publicapis.org/entries", "Metacommerce", GOOGLE_SERVICE_ACCOUNT_EMAIL as string, GOOGLE_PRIVATE_KEY as string).then(() => {
     console.log("Successful")
 });
+
 
 
